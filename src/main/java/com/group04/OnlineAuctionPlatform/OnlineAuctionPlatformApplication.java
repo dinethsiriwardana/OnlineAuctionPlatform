8e7f89fb44571672d1c132de6c2d294c3b3e271e
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2024-05-04 11:47:55 +0530

    First Commit

---
+++ src/main/java/com/group04/OnlineAuctionPlatform/OnlineAuctionPlatformApplication.java
@@ -0,0 +1,23 @@
+package com.group04.OnlineAuctionPlatform;
+
+import com.group04.OnlineAuctionPlatform.student.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class OnlineAuctionPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineAuctionPlatformApplication.class, args);
+	}
+
+//	@GetMapping
+
+}
